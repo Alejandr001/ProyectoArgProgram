b55eb4b193f942b60816974a2b1397508642d492
Francisco A <josefranciscoandrian@gmail.com>
2023-09-14 16:30:59 -0300

    Agregue los archivos
    me comi un paso en la creacion de repository

---
+++ src/universidadgrupo20/accesoADatos/InscripcionData.java
@@ -0,0 +1,15 @@
+
+package universidadgrupo20.accesoADatos;
+
+import java.sql.Connection;
+
+
+public class InscripcionData {
+    
+    private Connection con;
+    private MateriaData matData;
+    private AlumnoData aluData;
+    
+    
+    
+}
